9c9c38e36c7722410f2814210449b105381436ed
Yehuda Deutsch <yeh@uda.co.il>
2017-11-07 08:19:17 +0200

    Add json payload support
    Do something in @18Minuten

--- djext/http/request.py
+++ djext/http/request.py
@@ -1,8 +1,32 @@
+import json
+import re
+
 from django.core.handlers.wsgi import WSGIRequest
 
 
 class ExtendedWSGIRequest(WSGIRequest):
-    """
-    This is a placeholder for an extended Request object
-    """
-    pass
+    json_mime_type_regex = r'^application/(.*\+)?json$'
+
+    def _load_json(self):
+        self._json = {}
+        if self.method != 'POST':
+            return
+        if self._read_started and not hasattr(self, '_body'):
+            self._post_parse_error = True
+            return
+
+        if re.match(self.json_mime_type_regex, self.content_type):
+            try:
+                self._json = json.loads(self._body)
+            except:
+                self._post_parse_error = True
+
+    def _get_json(self):
+        if not hasattr(self, '_json'):
+            self._load_json()
+        return self._json
+
+    def _set_json(self, json):
+        self._json = json
+
+    json = property(_get_json, _set_json)
