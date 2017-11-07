fd84484353b32b059a94c0aa0eb1411d8d68eaa4
Yehuda Deutsch <yeh@uda.co.il>
2017-11-07 18:37:30 +0200

    Bump up version
    * Fix data to allow empty responses
    * Reorder responses by status code

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='djext',
-    version='0.0.5a1',
+    version='0.0.6a1',
     description='Quick tools for Django',
     author='Yehuda Deutsch',
     author_email='yeh@uda.co.il',
