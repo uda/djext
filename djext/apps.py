aaec4f2108bb98c61c2786f77cb7cf0efc024eee
Yehuda Deutsch <yeh@uda.co.il>
2017-11-08 08:11:04 +0200

    Mkae request_class configurable
    Do something in @18Minuten

--- djext/apps.py
+++ djext/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
-from django.core.handlers.wsgi import WSGIHandler
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.handlers.wsgi import WSGIHandler, WSGIRequest
 
 from .http.request import ExtendedWSGIRequest
 
@@ -9,4 +11,17 @@ class DjangoExtendedConfig(AppConfig):
     verbose_name = 'Quick tools for Django'
 
     def ready(self):
-        WSGIHandler.request_class = ExtendedWSGIRequest
+        WSGIHandler.request_class = self.get_request_class()
+
+    def get_request_class(self):
+        request_class = None
+        custom_request_class = getattr(settings, 'DJEXT_REQUEST_CLASS', None)
+        if custom_request_class and '.' in custom_request_class:
+            module_name, class_name = custom_request_class.rsplit('.', 1)
+            module = __import__(module_name, fromlist=[class_name])
+            request_class = getattr(module, class_name)
+        if not request_class:
+            request_class = ExtendedWSGIRequest
+        if not issubclass(request_class, WSGIRequest):
+            raise ImproperlyConfigured('The custom request class must extend the generic WSGIRequest')
+        return request_class
