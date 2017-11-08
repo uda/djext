9b08e835108373d5c989d4707fc92c5860e34c21
Yehuda Deutsch <yeh@uda.co.il>
2017-11-08 08:15:51 +0200

    Move the default to the settings get default
    Do something in @18Minuten

--- djext/apps.py
+++ djext/apps.py
@@ -3,8 +3,6 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from django.core.handlers.wsgi import WSGIHandler, WSGIRequest
 
-from .http.wsgi import ExtendedWSGIRequest
-
 
 class DjangoExtendedConfig(AppConfig):
     name = 'djext'
@@ -15,13 +13,11 @@ class DjangoExtendedConfig(AppConfig):
 
     def get_request_class(self):
         request_class = None
-        custom_request_class = getattr(settings, 'DJEXT_REQUEST_CLASS', None)
+        custom_request_class = getattr(settings, 'DJEXT_REQUEST_CLASS', 'djext.http.wsgi.ExtendedWSGIRequest')
         if custom_request_class and '.' in custom_request_class:
             module_name, class_name = custom_request_class.rsplit('.', 1)
             module = __import__(module_name, fromlist=[class_name])
             request_class = getattr(module, class_name)
-        if not request_class:
-            request_class = ExtendedWSGIRequest
         if not issubclass(request_class, WSGIRequest):
             raise ImproperlyConfigured('The custom request class must extend the generic WSGIRequest')
         return request_class
