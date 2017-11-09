301112121370790555a7af512ab50f89475a30f0
Yehuda Deutsch <yeh@uda.co.il>
2017-11-09 15:15:00 +0200

    Bump up version
    * Add json to request on PUT and PATCH requests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='djext',
-    version='0.1.1',
+    version='0.1.2',
     description='Quick tools for Django',
     author='Yehuda Deutsch',
     author_email='yeh@uda.co.il',
