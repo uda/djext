20894f13876b542be4997cd1f2ca9e5797133271
Yehuda Deutsch <yeh@uda.co.il>
2017-11-09 16:33:03 +0200

    Bump up version
    * Fix body reading

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setup(
     name='djext',
-    version='0.1.3',
+    version='0.1.4',
     description='Quick tools for Django',
     author='Yehuda Deutsch',
     author_email='yeh@uda.co.il',
