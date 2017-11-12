f29e46e890f07869e1ef9285b84f6b4766d1d913
Yehuda Deutsch <yeh@uda.co.il>
2017-11-12 15:02:12 +0200

    Add form_invalid basic handler

--- djext/json/views.py
+++ djext/json/views.py
@@ -1,3 +1,6 @@
+from .http.response import JsonResponseBadRequest
+
+
 class JsonFormMixin(object):
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -5,3 +8,10 @@ class JsonFormMixin(object):
                 and hasattr(self.request, 'json') and self.request.json:
             kwargs['data'] = self.request.json
         return kwargs
+
+    def form_invalid(self, form):
+        errors = {
+            field: [error for data in error_list.data for error in data]
+            for field, error_list in form.errors.items()
+        }
+        return JsonResponseBadRequest({'errors': errors})
