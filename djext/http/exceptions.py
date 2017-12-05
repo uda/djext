c6c77da7218f1b592a9537fd0b27c4af68c03480
Yehuda Deutsch <yeh@uda.co.il>
2017-12-05 11:21:16 +0200

    Add registry usage to HttpError

--- djext/http/exceptions.py
+++ djext/http/exceptions.py
@@ -1,7 +1,27 @@
 class HttpError(Exception):
+    _error_class_map = {}
     status_code = None
 
-    def __init__(self, status=None, reason=None, *args: object) -> None:
+    @classmethod
+    def register_error_class(cls, status_code, error_class, override_existing=False):
+        try:
+            status_code = int(status_code)
+        except:
+            raise ValueError('Status code must be an integer')
+        if status_code in cls._error_class_map and not override_existing:
+            raise KeyError(f'Status code {status_code} is already registered')
+        if not issubclass(error_class, cls):
+            raise ValueError(f'Error class must be a subclass of HttpError')
+        cls._error_class_map[status_code] = error_class
+
+    @classmethod
+    def with_status_code(cls, status_code, *args, **kwargs):
+        if status_code in cls._error_class_map:
+            return cls._error_class_map[status_code](*args, **kwargs)
+        else:
+            return cls(status_code, *args, **kwargs)
+
+    def __init__(self, status=None, reason=None, *args, **kwargs):
         if status is not None:
             try:
                 self.status_code = int(status)
@@ -12,13 +32,13 @@ class HttpError(Exception):
             raise ValueError('HTTP status code must be an integer from 100 to 599.')
 
         self.reason = reason
-        super().__init__(*args)
+        super().__init__(*args, **kwargs)
 
 
 class HttpErrorRedirectBase(HttpError):
-    def __init__(self, redirect_to, *args: object) -> None:
+    def __init__(self, redirect_to, *args, **kwargs):
         self.location = redirect_to
-        super().__init__(*args)
+        super().__init__(*args, **kwargs)
 
 
 class HttpErrorPermanentRedirect(HttpErrorRedirectBase):
@@ -55,3 +75,18 @@ class HttpErrorGone(HttpError):
 
 class HttpErrorInternalServerError(HttpError):
     status_code = 500
+
+
+_status_code_map = (
+    (301, HttpErrorPermanentRedirect),
+    (302, HttpErrorRedirect),
+    (400, HttpErrorBadRequest),
+    (401, HttpErrorUnauthorized),
+    (403, HttpErrorForbidden),
+    (404, HttpErrorNotFound),
+    (405, HttpErrorNotAllowed),
+    (410, HttpErrorGone),
+    (500, HttpErrorInternalServerError),
+)
+for status_code, error_class in _status_code_map:
+    HttpError.register_error_class(status_code, error_class)
