3fcecf94efb4bbdb323a676152650965b2b1db62
Yehuda Deutsch <yeh@uda.co.il>
2018-01-20 22:18:55 +0200

    Use settings for blame base model config

--- djext/blame/models.py
+++ djext/blame/models.py
@@ -1,13 +1,20 @@
 from django.contrib.auth import get_user_model
+from django.conf import settings
 from django.db import models
 
 UserModel = get_user_model()
 
+on_delete = getattr(settings, 'DJEXT_BLAME_ON_DELETE', models.SET_NULL)
+if hasattr(settings, 'DJEXT_BLAME_NULLABLE'):
+    null = bool(getattr(settings, 'DJEXT_BLAME_NULLABLE'))
+else:
+    null = on_delete == models.SET_NULL
+
 
 class BaseBlameModel(models.Model):
-    created_by = models.ForeignKey(UserModel, related_name='+', on_delete=models.SET_NULL)
+    created_by = models.ForeignKey(UserModel, related_name='+', on_delete=on_delete, null=null)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_by = models.ForeignKey(UserModel, related_name='+', on_delete=models.SET_NULL)
+    updated_by = models.ForeignKey(UserModel, related_name='+', on_delete=on_delete, null=null)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
