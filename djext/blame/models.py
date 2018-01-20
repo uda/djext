41ed92298020e1c50ae752c3515f7b4d514fa367
Yehuda Deutsch <yeh@uda.co.il>
2018-01-20 22:05:25 +0200

    Add on_delete arg, should now work with Django>=2.0

--- djext/blame/models.py
+++ djext/blame/models.py
@@ -5,9 +5,9 @@ UserModel = get_user_model()
 
 
 class BaseBlameModel(models.Model):
-    created_by = models.ForeignKey(UserModel, related_name='+')
+    created_by = models.ForeignKey(UserModel, related_name='+', on_delete=models.SET_NULL)
     created_on = models.DateTimeField(auto_now_add=True)
-    updated_by = models.ForeignKey(UserModel, related_name='+')
+    updated_by = models.ForeignKey(UserModel, related_name='+', on_delete=models.SET_NULL)
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
